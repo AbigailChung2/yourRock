25ef4d968c5b1c7ec1347a99bd90ae3293450691
Abigail Chung <achung40@stuy.edu>
2022-06-29 00:02:52 -0400

    Add actions and outlets for first three view controllers, menu at bottom

---
+++ yourRock/TypeScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TypeScreenViewController.swift
+//  yourRock
+//
+//  Created by Sun A Kim on 6/28/22.
+//
+
+import UIKit
+
+class TypeScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var displayPromptLabel: UILabel!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
