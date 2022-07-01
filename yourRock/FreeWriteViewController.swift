6ceb3e587f82b5b05a6d922d3db018b7e17fa10d
AbigailChung2 <108353868+AbigailChung2@users.noreply.github.com>
2022-06-30 20:53:04 -0400

    Add unwind segue for free write view controller

---
+++ yourRock/FreeWriteViewController.swift
@@ -0,0 +1,42 @@
+//
+//  FreeWriteViewController.swift
+//  yourRock
+//
+//  Created by Sun A Kim on 6/30/22.
+//
+
+
+import UIKit
+import CoreData
+
+class FreeWriteViewController: UIViewController {
+    
+    @IBOutlet weak var typingField: UITextView!
+    @IBOutlet weak var displayPromptLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        let entry = EntryCD(context : context)
+        
+        entry.written = typingField.text
+        
+        appDelegate.saveContext()
+        backToStart(_sender: (Any).self)
+    }
+    
+    
+    @IBAction func backToStart(_sender: Any) {
+            performSegue(withIdentifier: "unwind", sender: self)
+        }
+    
+}
